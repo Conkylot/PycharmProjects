171c2306e7a33061dc90ce1f8301d612a4f560c6
Conkylot <40263009+Conkylot@users.noreply.github.com>
2018-06-19 14:08:39 +0300

    Init
    Connect method

---
+++ PySQL/PYSQL.py
@@ -0,0 +1,8 @@
+import pypyodbc
+
+SQLconnect = pypyodbc.connect('driver={SQL server};'
+
+                              'server=DESKTOP-MMDDPMA\SQLEXPRESS;'
+
+                               'Database=AdventureWorks2017')
+
