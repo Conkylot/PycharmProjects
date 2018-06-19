daeba98b7ff0ec25e6339d6da6fd29d99f7fdc16
Conkylot <40263009+Conkylot@users.noreply.github.com>
2018-06-19 16:40:29 +0300

    pre-final SQL
    1

--- PySQL/PYSQL.py
+++ PySQL/PYSQL.py
@@ -2,33 +2,31 @@
 
 import pypyodbc
 
-SQLserver="DESKTOP-MMDDPMA\SQLEXPRESS"
-SQLDatabase="AdventureWorks2017"
+SQLserver = "DESKTOP-MMDDPMA\SQLEXPRESS"
+SQLDatabase = "AdventureWorks2017"
 SQLconnect = pypyodbc.connect('driver={SQL server};'
 
-                              'server='+ SQLserver + ';'
+                              'server=' + SQLserver + ';'
 
                               'Database=' + SQLDatabase + ' ;')
 
-cursor = connection.cursor()
+cursor = SQLconnect.cursor()
 
 
 SQLQuery = ("""
 
-SELECT TOP (1000) [AddressID]
-      ,[AddressLine1]
-      ,[AddressLine2]
-      ,[City]
-      ,[StateProvinceID]
-      ,[PostalCode]
-      ,[SpatialLocation]
-      ,[rowguid]
-      ,[ModifiedDate]
-  FROM [AdventureWorks2017].[Person].[Address]
+SELECT addressline1
+     
+      [City]
+      FROM [AdventureWorks2017].[Person].[Address]
 
-""""")
+""")
 
 cursor.execute(SQLQuery)
 
-results = cursor.fetchall()
 
+results = cursor.fetchall()
+print(results)
+
+
+SQLconnect.close()
